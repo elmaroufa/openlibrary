bf430ddfce083205f3bda37698f63e545d0e7998
elmaroufa <elmaroufa@gmail.com>
2021-11-21 21:44:45 +0100

    fisrt commit module

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Library Management',
+    'description' : 'Manage library book catalogue and content lending.',
+    'author' : 'saly abbo',
+    'data' : [
+        'security/library_security.xml',
+        'security/ir.model.access.csv'
+        'views/library_menu.xml'
+    ],
+    'depends' : ['base'],
+    'application' : True,
+}
