c177f06b3cb42a0c61bb23a1ef2d837b9346b4d3
elmaroufa <elmaroufa@gmail.com>
2021-11-22 12:21:02 +0100

    Add validation isbn in model

--- models/library_book.py
+++ models/library_book.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.exceptions import Warning
 
 class Book(models.Model):
     _name="library.book"
@@ -11,3 +12,23 @@ class Book(models.Model):
     image = fields.Binary('Cover')
     published_id = fields.Many2one('res.partner', string='Published')
     author_ids = fields.Many2many('res.partner', string="Authors")
+
+    @api.multi
+    def _check_isbn(self):
+        self.ensure_one()
+        digits = [int(x) for x in self.isbn if x.isdigit()]
+        if len(digits) == 13:
+            ponderations = [1, 3] * 6
+            terms = [a * b for a, b in zip(digits[:12], ponderations)]
+            remain = sum(terms) % 10
+            check = 10 - remain if remain != 0 else 0
+            return digits[-1] == check
+    
+    @api.multi
+    def button_check_isbn(self):
+        for book in self:
+            if not book.isbn:
+                raise Warning('Please provide an ISBN for %s' % book.name)
+            if book.isbn and not book._check_isbn():
+                raise Warning('%s is an invalid ISBN' % book.isbn)
+        return True
